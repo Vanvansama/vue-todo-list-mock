c8e252da5111b36a79abb2e043fec1b0632d59b4
LIUST3 <stefan.liu@oocl.com>
2019-07-23 17:08:13 +0800

    [stefan] init:function change and update todo item to mockAPI.io

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="listItem">
-    <p>
+    <p v-if="!edit">
       <input type="checkbox" class="checkbox" @click="handleCheck" v-model="status" />
-      <span :class="check" >{{item.todo}}</span>
+      <span :class="check" @dblclick="handleEdit">{{item.todo}}</span>
     </p>
-    <!-- <p v-else="db">
-      <input @keyup:enter=""/>
-    </p> -->
+    <p v-else>
+      <input v-model="input" @keyup.enter="handleEdit"/>
+    </p>
   </div>
 </template>
 
@@ -19,17 +19,25 @@ export default {
   data() {
     return {
       status: this.item.status,
+      edit: false,
+      input: ""
     };
   },
   methods: {
     handleCheck() {
       this.status = !this.status;
-      this.$store.dispatch('updateTodo',{...this.item,status:this.status})
+      this.$store.dispatch("updateTodo", { ...this.item, status: this.status });
+    },
+    handleEdit() {
+      if (this.input !== "") {
+        this.$store.dispatch("updateTodo", { ...this.item, todo: this.input });
+      }
+      this.edit = !this.edit;
     }
   },
   computed: {
     check() {
-      return this.status ? "check" : "none"
+      return this.status ? "check" : "none";
     }
   }
 };
